5a677810e3c844d7244d703e4540d3f76ae8ebba
Brian Sterner <brsterner@gmail.com>
2013-05-19 13:19:54 +1000

    Initial commit'
    cd
    quit
    q
    home
    git
    help

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '51f2a8f44064a1ed340a3617f4af17ca084ce3056c6e8fc9475ff44275054661801c5f9fe3a2e2855ba482995ba7eed11bb64580870114e93ff26ad618ff1ebf'
